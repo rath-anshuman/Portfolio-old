84e3ed226deadef3f57fe6b9f54bd76ce583ad46
Anshuman Rath <anshumanrath030@gmail.com>
2024-02-26 10:18:26 +0530

    initial

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+
+ a=true;
+ var typed = new Typed('#element', {
+      strings: ['Hello', '  I am adarsha nanada. ','i am a web coder','i am a fullstack python devloper'],
+      typeSpeed: 90,
+      loop:true
+    });
+ 
+
+
+
+
