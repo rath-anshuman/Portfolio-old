79fe4e1f8cc67ac545c533701a2dabaeb9e04372
Anshuman Rath <anshumanrath030@gmail.com>
2024-02-26 23:43:44 +0530

    .

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
  a=true;
  var typed = new Typed('#element', {
-      strings: ['Python Developer','Backend Developer','Django Developer','Projects Lead','Visit Github & Linked In'],
+      strings: ['Python Developer','Backend Developer','Django Developer','Projects Lead','Visit me on Github | Linked In '],
       typeSpeed: 80,
       loop:true
     });
