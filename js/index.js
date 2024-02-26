8544b1f889f8421e673496d4b7e0073646163830
Anshuman Rath <anshumanrath030@gmail.com>
2024-02-26 17:52:27 +0530

    developed first page

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
  a=true;
  var typed = new Typed('#element', {
-      strings: ['Python Developer','Backend Developer','Django Developer','Projects Lead'],
+      strings: ['Python Developer','Backend Developer','Django Developer','Projects Lead','Visit Github & Linked In'],
       typeSpeed: 90,
       loop:true
     });
